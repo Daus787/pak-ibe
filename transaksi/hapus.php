b17cc75927e84c183335dbd774350b2287273fda
dillah378 <nfadhillah105@gmail.com>
2026-03-05 10:05:39 +0800

    baru lagi

--- transaksi/hapus.php
+++ transaksi/hapus.php
@@ -1,14 +1,26 @@
-<?php 
-include "../config/koneksi.php";
+<?php
+// 1. Panggil koneksi database
+include '../config/koneksi.php';
 
+// 2. Cek apakah parameter penjualanID ada
 if (isset($_GET['penjualanID'])) {
+
+  // 3. Tangkap ID transaksi dari URL
   $penjualanID = $_GET['penjualanID'];
+  $nama = $_GET['nama'] ?? '';
 
-  $query = mysqli_query($koneksi, "DELETE FROM penjualan WHERE penjualanID = '$penjualanID'");
+  // 4. Query hapus data transaksi
+  $query = mysqli_query(
+    $koneksi,
+    "DELETE FROM penjualan WHERE penjualanID = '$penjualanID'"
+  );
 
+  // 5. Cek hasil query
   if ($query) {
-    header("Location: index_trans.php?pesan=berhasil");
+    header("Location: index_trans.php?status_hapus=sukses&nama=" . urlencode($nama));
   } else {
-    die("Delete gagal: " . mysqli_error($koneksi));
+    header("Location: index_trans.php?status_hapus=gagal&nama=" . urlencode($nama));
   }
+} else {
+  die("Akses dilarang...");
 }
